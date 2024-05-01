30ec7cd7c85715bf0273cd8810cce7d01c3d167b
Sathwick Paladugu <65240518+sathwick9@users.noreply.github.com>
2024-05-01 15:22:17 -0400

    Time: 1402 ms (41.26%), Space: 0B (100%) - LeetHub

---
+++ 1934-confirmation-rate/1934-confirmation-rate.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+select s.user_id, case when c.time_stamp is null 
+then 0.00 
+else Round(sum(c.action="confirmed")/count(*),2)
+END as confirmation_rate
+from signups s 
+left join confirmations c
+on s.user_id = c.user_id
+group by s.user_id

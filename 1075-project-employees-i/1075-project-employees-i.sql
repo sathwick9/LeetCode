352add06f4152901feec36a93dad0d24a15ecbf8
Sathwick Paladugu <65240518+sathwick9@users.noreply.github.com>
2024-05-07 14:42:54 -0400

    Delete 1075-project-employees-i directory

--- 1075-project-employees-i/1075-project-employees-i.sql
+++
@@ -1,5 +0,0 @@
-# Write your MySQL query statement below
-select project_id, Round(avg(experience_years), 2) average_years from project p
-left join employee e 
-on p.employee_id = e.employee_id 
-group by project_id;

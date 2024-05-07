f1961bf16deb9e0d3fc91596cdc8e44263e75532
Sathwick Paladugu <65240518+sathwick9@users.noreply.github.com>
2024-05-07 15:10:17 -0400

    Delete 1633-percentage-of-users-attended-a-contest directory

--- 1633-percentage-of-users-attended-a-contest/1633-percentage-of-users-attended-a-contest.sql
+++
@@ -1,4 +0,0 @@
-# Write your MySQL query statement below
-select contest_id, ROUND(COUNT(user_id)*100 / (SELECT COUNT(user_id) FROM Users), 2) AS percentage from register
-group by contest_id
-order by percentage desc, contest_id asc

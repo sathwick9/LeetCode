523af66b6c413632523b6edb9d967023ea058d7f
Sathwick Paladugu <65240518+sathwick9@users.noreply.github.com>
2024-05-09 15:48:15 -0500

    Time: 1684 ms (7.69%), Space: 0B (100%) - LeetHub

--- 1789-primary-department-for-each-employee/1789-primary-department-for-each-employee.sql
+++ 1789-primary-department-for-each-employee/1789-primary-department-for-each-employee.sql
@@ -1,8 +1,11 @@
 # Write your MySQL query statement below
-select employee_id, department_id from employee 
-where primary_flag = 'Y'
-Union
-select employee_id, department_id from employee
-group by employee_id 
-having count(employee_id) = 1
-order by employee_id
+-- select employee_id, department_id from employee 
+-- where primary_flag = 'Y'
+-- Union
+-- select employee_id, department_id from employee
+-- group by employee_id 
+-- having count(employee_id) = 1
+-- order by employee_id
+
+select employee_id, department_id from 
+(select employee_id, department_id,primary_flag,count(department_id) over(partition by employee_id) as cnt from employee ) dept where cnt = 1 or primary_flag = 'Y'
